2072f20319f118f7a24f1dbc630ac47d3c564705
Tom Martensen <mail@tommartensen.de>
2018-06-05 18:49:35 +0200

    Enable spi by default

--- raspbian-setup.sh
+++ raspbian-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 passwd
+mkdir /home/pi/code
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y python-pip python3-pip git
+sudo apt-get install -y python3-pip python3-smbus i2c-tools git
 curl -fsSL get.docker.com | sh
 sudo usermod -aG docker pi
 sudo pip install docker-compose
@@ -10,7 +11,7 @@ sudo service ssh start
 
 sudo update-rc.d ssh defaults
 sudo update-rc.d ssh enable
-
-mkdir /home/pi/code
-
-echo "Done."
+sudo sed -i -e 's/\#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
+echo "Done. Restarting server..."
+sleep 10
+sudo reboot
